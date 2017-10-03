aba2bbdb77cfd22ff237030f0bc7de33b9d69a57
L0G3X <wt2080@gmail.com>
2017-10-03 14:46:38 +0900

    winsock

--- source/database/keilo_core.hpp
+++ source/database/keilo_core.hpp
@@ -2,6 +2,9 @@
 
 #include <string>
 #include <list>
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
 
 typedef std::pair<std::string, std::string> keilo_instance;
 typedef std::list<keilo_instance> keilo_record;
