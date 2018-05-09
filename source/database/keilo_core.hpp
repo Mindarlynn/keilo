0a2b31d5474e778cb7edaa1d7d571d220b91463f
L0G3X <wt2080@gmail.com>
2018-05-10 03:03:32 +0900

    deleted struct and function
    client struct is changed to tcp_client class. and operator== function deleted because code compares std::list<tcp_client*>::iterator directly.

--- source/database/keilo_core.hpp
+++ source/database/keilo_core.hpp
@@ -8,21 +8,3 @@
 
 using keilo_instance = std::pair<std::string, std::string>;
 using keilo_record = std::list<keilo_instance>;
-
-static bool operator==(const SOCKADDR_IN a, const SOCKADDR_IN b)
-{
-	return a.sin_addr.S_un.S_addr == b.sin_addr.S_un.S_addr &&
-		a.sin_port == b.sin_port;
-}
-
-typedef struct
-{
-	/**
-	 * \brief Client's socket.
-	 */
-	SOCKET socket;
-	/**
-	 * \brief Client's address.
-	 */
-	SOCKADDR_IN address;
-} client;
